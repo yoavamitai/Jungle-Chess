15ade0ee539a3012e54a8f7d26065288907b81fa
yoavamitai <yoav.disni03@gmail.com>
2021-12-08 14:34:04 +0200

    Start of Main menu, titles

--- consts.py
+++ consts.py
@@ -2,3 +2,6 @@ import pygame as pg
 class Consts:
     pg.init()
     button_font = pg.font.Font('button_font.ttf', 30)
+    main_title_font = pg.font.Font('main_title_font.ttf', 50)
+    sub_title_font = pg.font.Font('main_title_font.ttf', 30)
+    label_font = pg.font.Font('main_title_font.ttf', 15)
--- main_menu.py
+++ main_menu.py
@@ -13,18 +13,31 @@ class MainMenu:
 
         # Screen Layout
         self.display = pg.display.set_mode((1200, 750), 0, 32)
-        self.pvp_button = Button("#B8C6DB", 100, 100, 200, 100, border_radius=10, text="Play PvP", font=Consts.button_font)
-        self.pve_button = Button("#B8C6DB", 100, 250, 200, 100, border_radius=10, text="Play PvE", font=Consts.button_font)
-        self.instuctions_button = Button("#B8C6DB", 100, 400, 200, 100, border_radius=10, text="Instructions", font=Consts.button_font)
-        self.quit_button = Button("#B8C6DB", 100, 550, 200, 100, border_radius=10, text="Quit", font=Consts.button_font)
+        self.pvp_button = Button("#B8C6DB", 60, 150, 200, 100, border_radius=10, text="Play PvP", font=Consts.button_font)
+        self.pve_button = Button("#B8C6DB", 60, 300, 200, 100, border_radius=10, text="Play PvE", font=Consts.button_font)
+        self.instuctions_button = Button("#B8C6DB", 60, 450, 200, 100, border_radius=10, text="Instructions", font=Consts.button_font)
+        self.quit_button = Button("#B8C6DB", 60, 600, 200, 100, border_radius=10, text="Quit", font=Consts.button_font)
         self.buttons = [self.pvp_button, self.pve_button, self.instuctions_button, self.quit_button]
+        
+        self.texts = [Consts.main_title_font.render('Dou Shou Qi', True, (0,0,0)),
+        Consts.sub_title_font.render('Jungle Chess', True, (0,0,0)),
+        Consts.label_font.render('by Yoav Amitai', True, (0,0,0))]
+
+        self.text_rects = [self.texts[0].get_rect(center=(200, 50)),
+        self.texts[1].get_rect(center=(260, 85)),
+        self.texts[2].get_rect(center=(290, 110))]
+
+        
+        
         pg.display.update()
         time.sleep(0.01)
         self.display.fill((235, 235, 235))
 
         for button in self.buttons:
             button.draw(self.display)
-        
+
+        for i in range(3):
+            self.display.blit(self.texts[i], self.text_rects[i])
         self.main_menu_loop()
     
     def main_menu_loop(self) -> None:
