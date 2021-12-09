d2115aa291488cde23a4db0a1bc0432d6b49cc57
yoavamitai <yoav.disni03@gmail.com>
2021-12-09 07:55:48 +0200

    Removed __pycache__, moved fonts into folder 'assets'

--- consts.py
+++ consts.py
@@ -1,7 +1,7 @@
 import pygame as pg
 class Consts:
     pg.init()
-    button_font = pg.font.Font('button_font.ttf', 30)
-    main_title_font = pg.font.Font('main_title_font.ttf', 50)
-    sub_title_font = pg.font.Font('main_title_font.ttf', 30)
-    label_font = pg.font.Font('main_title_font.ttf', 15)
+    button_font = pg.font.Font('assets/button_font.ttf', 30)
+    main_title_font = pg.font.Font('assets/main_title_font.ttf', 50)
+    sub_title_font = pg.font.Font('assets/main_title_font.ttf', 30)
+    label_font = pg.font.Font('assets/main_title_font.ttf', 15)
