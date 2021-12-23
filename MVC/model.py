27be2706b11d612f95b38a528f98bc625a4a6d2d
yoavamitai <yoav.disni03@gmail.com>
2021-12-23 09:52:51 +0200

    Created get_directions_to_river method, started added movement functionality Rank 8 game pieces. Created method head for check_moves_for_possible_directions

--- MVC/model.py
+++ MVC/model.py
@@ -13,6 +13,30 @@ class Model:
                  [6, 0, 0, 0, 0, 0, 7]]
         self.game_board = np.asarray(board, dtype=int)    # Turn board var into a numpy ndarray
     
+    def get_direction_to_river(self, pos):
+        directions = []
+        
+        # Check if piece is right to the river
+        if pos[0] in (3, 6) and 2 < pos[1] < 6:
+            directions.append('r')
+        
+        # check if piece is left to the river
+        if pos[0] in (0, 3) and 2 < pos[1] < 6:
+            directions.append('l')
+        
+        # Check if piece is above the river
+        if pos[0] in (1, 2, 4, 5) and pos[1] == 2:
+            directions.append('u')
+        
+        # Check if piece is below the river
+        if pos[0] in (1, 2, 4, 5) and pos[1] == 6:
+            directions.append('d')
+        
+        return directions
+    
+    def check_moves_for_possible_directions(self, position, rank, directions):
+        pass
+    
     def get_possible_moves(self, position):
         """returns possible moves of a game piece for a given position.
 
@@ -52,7 +76,24 @@ class Model:
             case 7: # Lion
                 pass
             case 8: # Elephant
-                pass
+                directions = self.get_direction_to_river(position)
+                if len(directions) == 0:
+                    # Piece is not adjacent to river
+                    if self.game_board[position[0], position[1] - 1] != 1 and position[1] - 1 >= 0:
+                        moves.append(tuple(position[0], position[1] - 1))   # Append move up
+                    
+                    if self.game_board[position[0], position[1] + 1] != 1 and position[1] + 1 < 9:
+                        moves.append(tuple(position[0], position[1] + 1))   # Append move down
+                    
+                    if self.game_board[position[0] - 1, position[1]] != 1 and position[0] - 1 >= 0:
+                        moves.append(tuple(position[0] - 1, position[1]))   # Append move left
+                    
+                    if self.game_board[position[0] + 1, position[1]] != 1 and position[0] + 1 < 7:
+                        moves.append(tuple(position[0] + 1, position[1]))   # Append move right
+                
+                else:
+                    pass          
+                
         
         return moves
     
