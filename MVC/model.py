52e3fd8adc84c887215283d0f5901962e8b09172
yoavamitai <yoav.disni03@gmail.com>
2021-12-26 12:04:32 +0200

    Changed get_possible_moves from match case to if statements

--- MVC/model.py
+++ MVC/model.py
@@ -48,51 +48,51 @@ class Model:
         current_rank = self.game_board[position[0], position[1]]    # Get current rank
         
         # Match case for every game piece rank.
-        match current_rank:
-            case 0:
-                return None     # return None if tile is empty (0 means empty)
-            case 1: # Rat
-                if self.game_board[position[0], position[1] - 1] in (0, 8) and position[1] - 1 >= 0:
-                    moves.append(tuple(position[0], position[1] - 1))    # Append move up
+        
+        if current_rank == 0:
+            return None     # return None if tile is empty (0 means empty)
+        elif current_rank == 1: # Rat
+            if self.game_board[position[0], position[1] - 1] in (0, 8) and position[1] - 1 >= 0:
+                moves.append(tuple(position[0], position[1] - 1))    # Append move up
+            
+            if self.game_board[position[0], position[1] + 1] in (0, 8) and position[1] < 9:
+                moves.append(tuple(position[0], position[1] + 1))     # Append move down
+            
+            if self.game_board[position[0] - 1, position[1]] in (0, 8) and position[0] >= 0:
+                moves.append(tuple(position[0] - 1, position[1]))     # Append move left
+            
+            if self.game_board[position[0] + 1, position[1]] in (0, 8) and position[0] < 7:
+                moves.append(tuple(position[0] + 1, position[1]))     # Append move right
+        elif current_rank == 2: # Cat
+            pass
+        elif current_rank == 3: # Dog
+            pass
+        elif current_rank == 4: # Wolf
+            pass
+        elif current_rank == 5: # Leopard
+            pass
+        elif current_rank == 6: # Tiger
+            pass
+        elif current_rank == 7: # Lion
+            pass
+        elif current_rank == 8: # Elephant
+            directions = self.get_direction_to_river(position)
+            if len(directions) == 0:
+                # Piece is not adjacent to river
+                if self.game_board[position[0], position[1] - 1] != 1 and position[1] - 1 >= 0:
+                    moves.append(tuple(position[0], position[1] - 1))   # Append move up
                 
-                if self.game_board[position[0], position[1] + 1] in (0, 8) and position[1] < 9:
-                    moves.append(tuple(position[0], position[1] + 1))     # Append move down
+                if self.game_board[position[0], position[1] + 1] != 1 and position[1] + 1 < 9:
+                    moves.append(tuple(position[0], position[1] + 1))   # Append move down
                 
-                if self.game_board[position[0] - 1, position[1]] in (0, 8) and position[0] >= 0:
-                    moves.append(tuple(position[0] - 1, position[1]))     # Append move left
+                if self.game_board[position[0] - 1, position[1]] != 1 and position[0] - 1 >= 0:
+                    moves.append(tuple(position[0] - 1, position[1]))   # Append move left
                 
-                if self.game_board[position[0] + 1, position[1]] in (0, 8) and position[0] < 7:
-                    moves.append(tuple(position[0] + 1, position[1]))     # Append move right
-            case 2: # Cat
-                pass
-            case 3: # Dog
-                pass
-            case 4: # Wolf
-                pass
-            case 5: # Leopard
-                pass
-            case 6: # Tiger
-                pass
-            case 7: # Lion
-                pass
-            case 8: # Elephant
-                directions = self.get_direction_to_river(position)
-                if len(directions) == 0:
-                    # Piece is not adjacent to river
-                    if self.game_board[position[0], position[1] - 1] != 1 and position[1] - 1 >= 0:
-                        moves.append(tuple(position[0], position[1] - 1))   # Append move up
-                    
-                    if self.game_board[position[0], position[1] + 1] != 1 and position[1] + 1 < 9:
-                        moves.append(tuple(position[0], position[1] + 1))   # Append move down
-                    
-                    if self.game_board[position[0] - 1, position[1]] != 1 and position[0] - 1 >= 0:
-                        moves.append(tuple(position[0] - 1, position[1]))   # Append move left
-                    
-                    if self.game_board[position[0] + 1, position[1]] != 1 and position[0] + 1 < 7:
-                        moves.append(tuple(position[0] + 1, position[1]))   # Append move right
-                
-                else:
-                    pass          
+                if self.game_board[position[0] + 1, position[1]] != 1 and position[0] + 1 < 7:
+                    moves.append(tuple(position[0] + 1, position[1]))   # Append move right
+            
+            else:
+                pass          
                 
         
         return moves
